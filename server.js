f35a77f41a36e0f9579d416778a8481f1d0144aa
jamietheminyard <45905995+jamietheminyard@users.noreply.github.com>
2021-09-22 16:51:53 -0500

    Fix port number

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // SERVER INFO
 const app = require("./app");
 
-let server = app.listen(8081, function () {
-   let host = server.address().address;
-   let port = server.address().port;
-   console.log("Listening at http://%s:%s", host, port)
-})
+let server = app.listen(process.env.PORT || 8081);
+
+//let server = app.listen(8081, function () {
+//   let host = server.address().address;
+//   let port = server.address().port;
+//   console.log("Listening at http://%s:%s", host, port)
+//})
