f190e92652974159cdb29058d249467c7a7018cc
jamietheminyard <45905995+jamietheminyard@users.noreply.github.com>
2021-09-23 16:44:11 -0500

    More updates from moving to Postgres

--- database.js
+++ database.js
@@ -1,26 +1,18 @@
 const { Client } = require('pg');
 
+// To connect from your local machine to the Heroku Postgres database
+// This value is updated by Heroku. If it fails to connect, get the updated value from heroku config -a fun-api
 let dbString = 'postgres://syjhlypnkldomc:f3779cfc60be2ed475f58323b3ab06ecfbc5d3b550323159275b62fdc4156149@ec2-34-233-105-94.compute-1.amazonaws.com:5432/db7cfi30u0pl4g';
 
 const db = new Client({
-    connectionString: process.env.DATABASE_URL || dbString,
+    connectionString: process.env.DATABASE_URL || dbString, // Use the Heroku DATABASE_URL or, if local, dbString
     ssl: {
         rejectUnauthorized: false
     }
 });
 
 console.log('Connecting to the Postgres database at ' + dbString);
-
 db.connect();
-
-db.query('SELECT * FROM orders limit 10;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-//    db.end();
-});
-
 console.log('Connected to the Postgres database.');
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // SERVER INFO
 const app = require("./app");
 
-let server = app.listen(process.env.PORT || 8081);
+let server = app.listen(process.env.PORT || 8081); // User Heroku's environment PORT or, if local, 8081
 
 //let server = app.listen(8081, function () {
 //   let host = server.address().address;
